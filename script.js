33c9fffc968f3d2d4080acdbb7f4b14dd1574e4a
Kazaki-Dev0 <kaique.s.campos@aluno.senai.br>
2025-06-24 11:17:24 -0300

    iniciante finalizado

--- script.js
+++ script.js
@@ -14,34 +14,21 @@ const input = document.getElementById("numero");
       }
     });
 
-    <!--ex2-->
+ //   ex2
 
-    let filtro = document.getElementById("filtro");
-    const lista = document.getElementById("lista");
-    const nomesOriginais = Array.from(lista.children).map(li => li.innerText);
-
-    filtro.addEventListener("filtro", function () {
-      const termo = filtro.value();
-
-      lista.innerHTML = "";
-
-      nomesOriginais.forEach(nome => {
-        const nomeLower = nome.toLowerCase();
-        const posicao = nomeLower.indexOf(termo);
-
-        const li = document.createElement("li");
-
-        if (termo && posicao !== -1) {
-          // separa a parte a ser destacada
-          const inicio = nome.substring(0, posicao);
-          const destaque = nome.substring(posicao, posicao + termo.length);
-          const fim = nome.substring(posicao + termo.length);
-
-          li.innerHTML = `${inicio}<strong>${destaque}</strong>${fim}`;
-        } else {
-          li.innerText = nome;
-        }
-
-        lista.appendChild(li);
-      });
-    });
+ const inputNome = document.getElementById('nome');
+ const lista = document.getElementById('lista');
+ 
+ inputNome.addEventListener('input', () => {
+   const texto = inputNome.value.toLowerCase();
+   const itens = lista.querySelectorAll('li')
+ 
+   itens.forEach(item => {
+     const nome = item.textContent.toLowerCase();
+     if (nome.includes(texto)) {
+       item.style.fontWeight = "bold";
+     } else {
+       item.style.fontWeight = "normal";
+     } 
+   });
+});
